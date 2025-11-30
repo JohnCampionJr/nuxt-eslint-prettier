589c9abc78103927ce1a64c0732ebab43b70c88d
John Campion Jr <john@brightshore.com>
2025-11-30 15:43:36 -0500

    missing files

---
+++ .prettierrc.js
@@ -0,0 +1,13 @@
+export default {
+  arrowParens: 'always',
+  bracketSameLine: false,
+  endOfLine: 'lf',
+  printWidth: 100,
+  singleQuote: true,
+  semi: false,
+  singleAttributePerLine: false,
+  trailingComma: 'all',
+  // until this plugin is updated to support Prettier v3.7
+  // plugins: ['prettier-plugin-classnames'],
+  plugins: ['./prettier.classnames.cjs'],
+}
